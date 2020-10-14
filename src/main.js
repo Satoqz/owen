f7501762811c74315ba83e2b2ef844b7c7a0cf13
Satoqz <satoqzzz@gmail.com>
2020-10-14 11:26:00 +0200

    eslint config update

--- src/main.js
+++ src/main.js
@@ -10,72 +10,79 @@ const instanceLock = app.requestSingleInstanceLock();
 
 let win;
 
-app.on("second-instance", () => {
-    if (win) {
-        if (win.isMinimized()) {
-            win.restore();
-        }
-        win.focus();
-    }
+app.on("second-instance", () =>
+{
+	if (win)
+	{
+		if (win.isMinimized())
+		{
+			win.restore();
+		}
+		win.focus();
+	}
 });
 
-if (!instanceLock) {
-    return app.quit();
+if (!instanceLock)
+{
+	return app.quit();
 }
 
-function createWindow() {
-    win = new BrowserWindow({
-        frame: false,
-        width: 900,
-        height: 600,
-        icon: iconpath,
-        resizable: false,
-        titleBarStyle: "hidden",
-        webPreferences: {
-            nodeIntegration: true,
-            webSecurity: true
-        },
-    });
+function createWindow()
+{
+	win = new BrowserWindow({
+		frame: false,
+		width: 900,
+		height: 600,
+		icon: iconpath,
+		resizable: false,
+		titleBarStyle: "hidden",
+		webPreferences: {
+			nodeIntegration: true,
+			webSecurity: true
+		},
+	});
 
-    win.setMenu(null);
+	win.setMenu(null);
 
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
-        slashes: true,
-    })).then(() => console.log("window finished loading"));
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, "index.html"),
+		protocol: "file:",
+		slashes: true,
+	})).then(() => console.log("window finished loading"));
 
-    const appIcon = new Tray(iconpath);
+	const appIcon = new Tray(iconpath);
 
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: "Quit",
-            click() {
-                app.isQuiting = true;
-                app.quit();
-                app.exit();
-            },
-        },
-    ]);
+	const contextMenu = Menu.buildFromTemplate([
+		{
+			label: "Quit",
+			click()
+			{
+				app.isQuiting = true;
+				app.quit();
+				app.exit();
+			},
+		},
+	]);
 
-    appIcon.on("click", () => win.show());
+	appIcon.on("click", () => win.show());
 
-    appIcon.on("right-click", () => appIcon.setContextMenu(contextMenu));
+	appIcon.on("right-click", () => appIcon.setContextMenu(contextMenu));
 
-    win.on("close", (event) => {
-        event.preventDefault();
-        win.hide();
-    });
-        
-    ipcMain.on("presence-update", (event, arg) => setActivity(arg.line1, arg.line2, arg.hover, arg.imageKey));
-        
-    ipcMain.on("close-app", () => win.hide());
+	win.on("close", (event) =>
+	{
+		event.preventDefault();
+		win.hide();
+	});
 
-    ipcMain.on("error-message", () => dialog.showMessageBox({
-        message: "Make sure that you filled everything in with more than one character and picked an image",
-        type: "info",
-        title: "oopsie"
-    }));
+	ipcMain.on("presence-update", (event, arg) => setActivity(arg.line1, arg.line2, arg.hover, arg.imageKey));
+
+	ipcMain.on("close-app", () => win.hide());
+
+	ipcMain.on("error-message", () => dialog.showMessageBox({
+		message: "Make sure that you filled everything in with more than one character and picked an image",
+		type: "info",
+		title: "oopsie"
+	}));
 }
 
 const clientId = "727887407491317910";
@@ -85,25 +92,27 @@ const startTimestamp = new Date();
 
 DiscordRPC.register(clientId);
 
-async function setActivity(line1, line2, hover, imageKey) {
+async function setActivity(line1, line2, hover, imageKey)
+{
 
-    if (!rpc || !win) return;
+	if (!rpc || !win) return;
 
-    rpc.setActivity({
-        details: line1,
-        state: line2,
-        startTimestamp,
-        largeImageKey: imageKey,
-        largeImageText: hover,
-        instance: true,
-        spectateSecret: "owen",
-    });
+	rpc.setActivity({
+		details: line1,
+		state: line2,
+		startTimestamp,
+		largeImageKey: imageKey,
+		largeImageText: hover,
+		instance: true,
+		spectateSecret: "owen",
+	});
 }
 
 rpc.login({ clientId }).catch(console.error);
 
-app.on("ready", () => {
-    autoUpdater.checkForUpdatesAndNotify();
-    setInterval(() => autoUpdater.checkForUpdatesAndNotify(), 60*60*1000);
-    createWindow();
+app.on("ready", () =>
+{
+	autoUpdater.checkForUpdatesAndNotify();
+	setInterval(() => autoUpdater.checkForUpdatesAndNotify(), 60*60*1000);
+	createWindow();
 });
--- src/renderer.js
+++ src/renderer.js
@@ -2,47 +2,54 @@ const { ipcRenderer } = require("electron");
 const fs = require("fs");
 const path = require("path");
 // eslint-disable-next-line no-undef
-let vue = new Vue({
-    el: "#app",
-    data: {
-        assets: [],
-        line1: "",
-        line2: "",
-        hover: "",
-        pickedImage: "",
-    },
-    methods: {
-        assetPath: function(key) {
-            return "discordassets/" + key + ".png";
-        },
-        fetchAssets: function() {
-            this.assets = fs.readdirSync(path.join(__dirname, "/discordassets")).filter(i => i.endsWith(".png")).map(i => i.replace(".png", ""));
-        }
-    },
-    created: function() {
-        this.fetchAssets();
-    }
+const vue = new Vue({
+	el: "#app",
+	data: {
+		assets: [],
+		line1: "",
+		line2: "",
+		hover: "",
+		pickedImage: "",
+	},
+	methods: {
+		assetPath: function(key)
+		{
+			return "discordassets/" + key + ".png";
+		},
+		fetchAssets: function()
+		{
+			this.assets = fs.readdirSync(path.join(__dirname, "/discordassets")).filter(i => i.endsWith(".png")).map(i => i.replace(".png", ""));
+		}
+	},
+	created: function()
+	{
+		this.fetchAssets();
+	}
 });
 
-function requestRPCChange() {
-    if(vue.line1.length >= 2
+function requestRPCChange()
+{
+	if (vue.line1.length >= 2
         && vue.line2.length > 2
         && vue.hover.length > 2
         && vue.pickedImage
-    ) {
-        const data = {
-            line1: vue.line1,
-            line2: vue.line2,
-            hover: vue.hover,
-            imageKey: vue.pickedImage
-        };
-        ipcRenderer.send("presence-update", data);
-    }
-    else {
-        ipcRenderer.send("error-message");
-    }
+	)
+	{
+		const data = {
+			line1: vue.line1,
+			line2: vue.line2,
+			hover: vue.hover,
+			imageKey: vue.pickedImage
+		};
+		ipcRenderer.send("presence-update", data);
+	}
+	else
+	{
+		ipcRenderer.send("error-message");
+	}
 }
 
-function closeApp() {
-    ipcRenderer.send("close-app", "close");
+function closeApp()
+{
+	ipcRenderer.send("close-app", "close");
 }
